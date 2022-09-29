9803847efb865d8530e58fe14f80d220acace329
w0791155 <98964957+w0791155@users.noreply.github.com>
2022-09-28 23:37:21 -0400

    Assignment 2

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// WEB303 Assignment 2
+// Created By- Prabhjeet Singh
+// Student Number- w0791155 
+
+$(document).ready(function(){
+        $("a#prospect").click(function(){
+        $("#content").hide().load("prospect.html").slideDown(200); 
+        console.log("Prospect loaded");
+        });
+$("a#retain").click(function(){
+ $("#content").hide().load("retain.html").slideToggle(2000);
+ console.log("Retain loaded");
+});
+    $("a#convert").click(function(){
+    $("#content").hide().load("convert.html").slideToggle(1200);
+    console.log("Covert loaded");
+   });
+});
