4842e1047e1e68dafa428b6bba48ec26d33e45a5
w0791155 <98964957+w0791155@users.noreply.github.com>
2022-11-03 23:53:39 -0400

    updated

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-    $('.accordion').on('click', '.control', function () {
-        $(this).next().addClass('active').slideDown(1000); //add class and slide down and show content
-        $('.active').not($(this).next()).removeClass('active').slideUp(1000); // remove class and slide up and hide content
+    $('.accordion').on('click', '.accordion-control', function () {
+        $(this).next().addClass('active3').slideDown(1000); //add class and slide down and show content
+        $('.active3').not($(this).next()).removeClass('active3').slideUp(1000); // remove class and slide up and hide content
         $(this).next('.accordion-panel').show(); // if another panel is clicked, repeat lines 3 and 4
         console.log('please work qwq'); // crying for this code to work. 
 });
-    $('.accordion2').on('click', '.control2', function () {
-        $(this).next().addClass('tab-panel active').slideDown(1000); //add class and slide down and show content
-        $('.tab-panel active').not($(this).next()).removeClass('tab-panel active').slideUp(1000); // remove class and slide up and hide content
+    $('.accordion2').on('click', '.accordion-control2', function () {
+        $(this).next().addClass('active2').slideDown(1000); //add class and slide down and show content
+        $('.active2').not($(this).next()).removeClass('active2').slideUp(1000); // remove class and slide up and hide content
         $(this).next('.accordion-panel2').show(); // if another panel is clicked, repeat lines 3 and 4
         console.log('please work qwq on acc2'); // crying for this code to work. 
 });
